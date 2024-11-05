f1681737bdfe1617e518a820d4f8f93d0440091a
Andrej Mandli <andrej.mandli@betterask.erni>
2024-11-05 11:29:48 +0100

    Do not try to deserialize missing cache item

--- MasterData.Domain/Cache/IItemCache.cs
+++ MasterData.Domain/Cache/IItemCache.cs
@@ -8,9 +8,9 @@ namespace MasterData.Domain.Cache
 {
     public interface IItemCache
     {
-        public Task<T?> Get<T>(string key, CancellationToken cancellationToken);
+        public Task<T?> Get<T>(string key, CancellationToken cancellationToken) where T : class;
 
-        public Task Set<T>(string key, T item, CancellationToken cancellationToken);
+        public Task Set<T>(string key, T item, CancellationToken cancellationToken) where T : class;
 
         public Task Remove(string key, CancellationToken cancellationToken);
     }
--- MasterData.Domain/Cache/ItemCache.cs
+++ MasterData.Domain/Cache/ItemCache.cs
@@ -9,9 +9,14 @@ namespace MasterData.Domain.Cache
     {
         private const int ExpirationInSeconds = 10;
 
-        public async Task<T?> Get<T>(string key, CancellationToken cancellationToken)
+        public async Task<T?> Get<T>(string key, CancellationToken cancellationToken) where T : class
         {
             var rawItem = await cache.GetAsync(key, cancellationToken);
+
+            if (rawItem is null)
+            {
+                return null;
+            }
 
             var item = JsonSerializer.Deserialize<T>(rawItem);
 
@@ -20,7 +25,7 @@ namespace MasterData.Domain.Cache
 
         public Task Remove(string key, CancellationToken cancellationToken) => cache.RemoveAsync(key, cancellationToken);
 
-        public Task Set<T>(string key, T item, CancellationToken cancellationToken) => cache.SetAsync(key, Encoding.UTF8.GetBytes(JsonSerializer.Serialize(item)), new()
+        public Task Set<T>(string key, T item, CancellationToken cancellationToken) where T : class => cache.SetAsync(key, Encoding.UTF8.GetBytes(JsonSerializer.Serialize(item)), new()
         {
             AbsoluteExpiration = DateTime.Now.AddSeconds(ExpirationInSeconds)
         });
